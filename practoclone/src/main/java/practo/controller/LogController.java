07e2e605affcf9be7d46ada7f7800ea4f9f6c816
Md Farhan Sayeed <fs10956@gmail.com>
2021-12-15 18:35:49 +0530

    Registration module

--- practoclone/src/main/java/practo/controller/LogController.java
+++ practoclone/src/main/java/practo/controller/LogController.java
@@ -2,16 +2,21 @@ package practo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import practo.model.User;
+import practo.service.UserService;
 
 @Controller
 public class LogController {
+		@Autowired
+		private UserService userService;
 	
 	/* This method/request handler is only used to show the registration form */
 		@RequestMapping("/registeruser")
@@ -20,28 +25,12 @@ public class LogController {
 		}
 		
 		@RequestMapping(path = "/processform", method = RequestMethod.POST)
-		public String handleForm(
-				@RequestParam(name = "username",required = false) String userName,
-				@RequestParam("email") String userEmail,
-				@RequestParam("password") String userPassword,
-				Model model) 
+		public String handleForm(@ModelAttribute User user,Model model) 
 		{
-			User user=new User();
-			user.setUsername(userName);
-			user.setEmail(userEmail);
-			user.setPassword(userPassword);
 			System.out.println(user);
-			/*
-			System.out.println("User name is "+userName);
-			System.out.println("User email is "+userEmail);
-			System.out.println("User password is "+userPassword);
-			*/
-			//we can process the data here
-			
-			//model.addAttribute("name",userName);
-			//model.addAttribute("email",userEmail);
-			//model.addAttribute("password",userPassword);
-			model.addAttribute("user",user);
+			//process
+			int createUser = this.userService.createUser(user);
+			model.addAttribute("msg","User created with id "+createUser);
 			return "success";
 		}
 		/*
--- practoclone/src/main/webapp/WEB-INF/views/success.jsp
+++ practoclone/src/main/webapp/WEB-INF/views/success.jsp
@@ -1,13 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-    <%@page isELIgnored="false"%>
+<%@page isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>successfully registered</title>
 </head>
 <body>
+<h1 style="color: green">${msg }</h1>
+<hr>
 <h1>Welcome ${user.username} </h1>
 <h1>Your email address is ${user.email }</h1>
 <h1>Your password is ${user.password }</h1>
